3d56d06475c2d8ad8b18b4fe1682fe2f3334f82d
Jake Walker <jcw2865@gmail.com>
2020-02-13 20:31:58 -0600

    Combined contents of exercise.js model into workout.js model.

--- models/workout.js
+++ models/workout.js
@@ -2,23 +2,36 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    date: {
+    day: {
         type: Date,
         default: Date.now
     },
-
-    duration: {
-        type: Number
-    },
-
-    exercises: [{
-        type: Schema.Types.ObjectId,
-        ref: "Exercise"
-    }],
-
-
-});
+    exercises: [
+        {
+            type: {
+                type: String,
+            },
+            name: {
+                type: String,
+            },
+            duration: {
+                type: Number,
+            },
+            weight: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            },
+            distance: {
+                type: Number,
+            }
+        }
+    ]
+})
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
-
 module.exports = Workout;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 const db = require("./models");
 const app = express();
+// const seeder = require("./seeders/seed");
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,7 +14,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { use
 
 // Requiring our routes
 require("./routes/html-routes.js")(app);
-// require("./routes/api-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
